44622926e53da36b65695628deb2ef1d8a34271f
Quinn Slack <qslack@qslack.com>
2013-08-09 10:37:56 -0700

    fix ref to sourcegraph-nodejs-sample

--- animalRef-2.js
+++ animalRef-2.js
@@ -1,4 +1,4 @@
-var animal2 = require('nodejs-sample/animal');
+var animal2 = require('sourcegraph-nodejs-sample/animal');
 
 exports.MyDog2 = new animal2.Dog('alice2', 'Australian Shepherd2');
 exports.AnotherDog2 = new animal2.Dog('bob2', 'Corgi2');
