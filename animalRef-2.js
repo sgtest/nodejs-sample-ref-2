2b5bf543b465ab4650c6c7fad950dc81f6fdc7f4
Quinn Slack <qslack@qslack.com>
2013-08-09 09:27:43 -0700

    Add "-2"

---
+++ animalRef-2.js
@@ -0,0 +1,6 @@
+var animal2 = require('nodejs-sample/animal');
+
+exports.MyDog2 = new animal2.Dog('alice2', 'Australian Shepherd2');
+exports.AnotherDog2 = new animal2.Dog('bob2', 'Corgi2');
+
+console.log(exports.MyDog2, exports.AnotherDog2);
--- animalRef.js
+++
@@ -1,6 +0,0 @@
-var animal = require('nodejs-sample/animal');
-
-exports.MyDog = new animal.Dog('alice', 'Australian Shepherd');
-exports.AnotherDog = new animal.Dog('bob', 'Corgi');
-
-console.log(exports.MyDog, exports.AnotherDog);
